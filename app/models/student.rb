b8daa61ba9b27cf4913167465e73943877a35158
Ritesh Aravindra <riteshingaleshwar@gmail.com>
2023-03-09 02:02:15 +0530

    ad

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
     validates :first_name, :last_name, :email, presence: true
+    validates :first_name, :last_name, :email, presence: true
 end
+
+#Student Controller
